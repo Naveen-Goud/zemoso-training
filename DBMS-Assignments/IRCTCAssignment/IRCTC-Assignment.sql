2b1b1ca7abcb9f440665e28cf0e185074f3a0e71
naveen goud <naveengoud7799@gmail.com>
2023-05-18 11:40:04 +0530

    IRCTC Assignment review

---
+++ DBMS-Assignments/IRCTCAssignment/IRCTC-Assignment.sql
@@ -0,0 +1,37 @@
+
+ create database  EcommerceAssignment;
+ use EcommerceAssignment;
+ 
+  -- Creating the Hotel table
+CREATE TABLE Hotel (
+  hotel_id INT PRIMARY KEY,
+  hotel_name VARCHAR(255),
+  hotel_address VARCHAR(255),
+  other_hotel_details VARCHAR(255)
+);
+
+-- Creating the Customer table
+CREATE TABLE Customer (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(255),
+  customer_type VARCHAR(255),
+  other_customer_details VARCHAR(255)
+);
+
+-- Creating the Customer table
+CREATE TABLE Customer (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(255),
+  customer_type VARCHAR(255),
+  other_customer_details VARCHAR(255)
+);
+
+-- Creating the Booking table
+CREATE TABLE Booking (
+  booking_id INT PRIMARY KEY,
+  hotel_id INT,
+  customer_id INT,
+  FOREIGN KEY (hotel_id) REFERENCES Hotel(hotel_id),
+  FOREIGN KEY (customer_id) REFERENCES Customer(customer_id)
+);
+
